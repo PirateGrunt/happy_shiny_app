a8e9be09352be7fb9c30c2e563675353ad5824dd
PirateGrunt <captain@pirategrunt.com>
2019-06-18 14:20:28 -0400

    Navbar layout

--- app.R
+++ app.R
@@ -1,9 +1,37 @@
 library(shiny)
+library(tidyverse)
 
-ui <- fluidPage(
+tbl_avengers <- readr::read_csv('avengers.csv')
+
+ui <- navbarPage(
   title = "Happy Shiny"
+  , tabPanel(
+    'Table'
+    , tableOutput('out_avengers_table')
+  )
+  , tabPanel(
+    'Plot'
+    , plotOutput('out_avengers_plot')
+  )
 )
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  
+  output$out_avengers_table <-  renderTable({
+      tbl_avengers
+    }
+    , striped = TRUE
+  )
+  
+  output$out_avengers_plot <- renderPlot({
+    
+    tbl_avengers %>% 
+      ggplot(aes(Appearances, color = Gender)) +
+      geom_density() +
+      scale_x_log10()
+    
+  })
+  
+}
 
 shinyApp(ui, server)
