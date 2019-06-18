75e81b7f9873ba066115677a83c3a8f10b9f6b0a
PirateGrunt <captain@pirategrunt.com>
2019-06-18 16:02:09 -0400

    Initial (wrong) way to handle data

--- plot.R
+++ plot.R
@@ -7,21 +7,29 @@ tab_plot <- tabPanel(
     , 'Current?'
     , c('All', 'Current', 'Former'))
   
+  , tableOutput('out_agg_table')
   , plotOutput('out_avengers_plot')
 )
 
 expr_plot <- quote({
 
+  if (input$in_filter_current == 'All') {
+    tbl_filtered <- tbl_avengers
+  } else {
+    tbl_filtered <- tbl_avengers %>% 
+      filter(`Current?` == ifelse(input$in_filter_current == 'Current', 'YES', 'NO'))
+  }
+  
+  output$out_agg_table <- renderText({
+    
+    tbl_filtered %>% 
+      count(Gender)
+    
+  })
+  
   output$out_avengers_plot <- renderPlot({
     
-    if (input$in_filter_current == 'All') {
-      tbl_plot <- tbl_avengers
-    } else {
-      tbl_plot <- tbl_avengers %>% 
-        filter(`Current?` == ifelse(input$in_filter_current == 'Current', 'YES', 'NO'))
-    }
-    
-    tbl_plot %>% 
+    tbl_filtered %>% 
       ggplot(aes(Appearances, color = Gender)) +
       geom_density() +
       scale_x_log10()
